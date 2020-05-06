29393e3bcef7c88d31a8649e521e88279844c0ea
luyao61 <luyao912@gmail.com>
2020-05-05 17:08:31 -0700

    invoke solution from command line

--- Test.java
+++ Test.java
@@ -1,6 +1,26 @@
+import java.lang.reflect.*;
+
 public class Test {
-    public static void main(String[] args) {
-        Solution25 sol = new Solution25();
-        sol.test();
+    public static void main(String... args) {
+        try {
+            Class<?> c = Class.forName(args[0]);
+            LeetcodeSolution solution = (LeetcodeSolution) c.getDeclaredConstructor().newInstance();
+
+            Method testFunction = c.getDeclaredMethod("test");
+            System.out.format("invoking %s.test()%n", c.getName());
+            testFunction.invoke(solution);
+
+            // production code should handle these exceptions more gracefully
+        } catch (InstantiationException x) {
+            x.printStackTrace();
+        } catch (ClassNotFoundException x) {
+            x.printStackTrace();
+        } catch (NoSuchMethodException x) {
+            x.printStackTrace();
+        } catch (IllegalAccessException x) {
+            x.printStackTrace();
+        } catch (InvocationTargetException x) {
+            x.printStackTrace();
+        }
     }
 }
