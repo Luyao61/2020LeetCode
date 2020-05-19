8965d01473572930237882a2d280f41058cc1299
Luyao Zhou <luyao61@luyaos-mbp.lan>
2020-05-19 11:06:15 -0700

    diagonally sort matrix

---
+++ Solution1329.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+// https://leetcode.com/problems/sort-the-matrix-diagonally/
+class Solution1329 {
+    public int[][] diagonalSort(int[][] mat) {
+        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                map.putIfAbsent(i - j, new PriorityQueue<>());
+                map.get(i - j).offer(mat[i][j]);
+            }
+        }
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                mat[i][j] = map.get(i - j).poll();
+            }
+        }
+        return mat;
+    }
+}
